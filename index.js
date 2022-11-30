317977380122e09547188037fa0f045a7f32a1c7
certihimo <kalidahm3d@gmail.com>
2022-11-30 14:02:04 +0300

    complete

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(){
+    cats.push("Ralph")
+}
+function destructivelyPrependCat(name){
+    cats.unshift("Bob")
+}
+function destructivelyRemoveLastCat(){
+    cats.pop("Garfield")
+}
+function destructivelyRemoveFirstCat(){
+    cats.shift("Milo")
+}
+function prependCat(){
+    const newArray = [...cats]
+    newArray.unshift("Arnold")
+    return newArray
+}
+function appendCat(){
+    const newArray =[...cats,"Broom"]
+    return newArray
+}
+function removeFirstCat(){
+    var newArray = cats.slice(1)
+    return newArray
+}
+function removeLastCat(){
+    var newArray = ["Milo","Otis"]
+    return newArray
+}
